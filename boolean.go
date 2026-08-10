192a19127b09f33d1b27cea0afeeff776f86028c
Derylfabiensyah <derylfabien09@gmail.com>
2026-08-10 20:46:44 +0700

    menjelaskan tipe data boolean

---
+++ boolean.go
@@ -0,0 +1,12 @@
+/* 
+	tipe data Boolean yang hanya ada dua nilai yaitu true dan false
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+}
