96aa0d6737768f567c04121359bf3544aa167e48
Derylfabiensyah <derylfabien09@gmail.com>
2026-08-11 13:14:08 +0700

    menjelaskan operator perbandingan

---
+++ comparation-operation.go
@@ -0,0 +1,16 @@
+/*
+	operatior perbandingan sama seperti bahasa pemograman lain
+	ada > < >= <= == !=
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var name1 = "deryl"
+	var name2 = "deryl"
+
+	var result bool = name1 == name2
+	fmt.Println(result)
+}
